d711a9376380e381a51d69fb40ef3b445ec2ecc1
ScarletBlue <koreanboy1120@gmail.com>
2017-06-20 08:21:44 +0900

    Adding wall jump

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(SpriteRenderer))]
-public class MovementController : MonoBehaviour {
+public class MovementController : MonoBehaviour
+{
 
 	Rigidbody2D rb2d;
 	SpriteRenderer spriteRenderer;
@@ -22,25 +24,36 @@ public class MovementController : MonoBehaviour {
 	public float jumpVelocity;
 	public float doubleJumpVelocity;
 
-	bool onFloor;
-	bool performedDoubleJump;
-	bool movementDisabled;
+	[HideInInspector]
+	public bool onFloor;
+	[HideInInspector]
+	public bool performedDoubleJump;
+	[HideInInspector]
+	public bool movementDisabled;
+	/*
+	bool onWall;
+	bool performedWallJump;
+	public float wallJumpDistance = 1f;
+	*/
 
 	// Sprite Rendering
 
 	bool facingRight;
-	public bool IsFacingRight() {
+	public bool IsFacingRight()
+	{
 		return facingRight;
 	}
 
-	void Awake() {
+	void Awake()
+	{
 		rb2d = GetComponent<Rigidbody2D>();
 		spriteRenderer = GetComponent<SpriteRenderer>();
 
 		healthController = GetComponent<HealthController> ();
 	}
 
-	void Start() {
+	void Start()
+	{
 		movementDisabled = false;
 
 		facingRight = true;
@@ -48,28 +61,36 @@ public class MovementController : MonoBehaviour {
 		SetIfOnFloor();
 	}
 
-	void Update() {
+	void Update()
+	{
 		SetIfOnFloor();
 	}
 
-	public enum Direction {
+
+	public enum Direction
+	{
 		Left,
 		Right,
 		None
 	}
 
-	public void Move(Direction direction) {
-		if (healthController != null) {
-			if (healthController.GetHealth () <= 0) {
+	public void Move(Direction direction)
+	{
+		if (healthController != null)
+		{
+			if (healthController.GetHealth () <= 0)
+			{
 				return;
 			}
 		}
 
-		if (movementDisabled) {
+		if (movementDisabled)
+		{
 			return;
 		}
 
-		switch (direction) {
+		switch (direction)
+		{
 			case Direction.Left:
 				rb2d.velocity = new Vector2(-moveVelocity, rb2d.velocity.y);
 				break;
@@ -84,8 +105,10 @@ public class MovementController : MonoBehaviour {
 		SetSpriteDirection(direction);
 	}
 
-	void SetIfOnFloor() {
-		
+
+
+	void SetIfOnFloor()
+	{
 		Debug.DrawLine(transform.position, (transform.position + (Vector3.down * ((spriteRenderer.bounds.size.y/2)+0.1f))), Color.red, 0, false);
 		// Debug.DrawRay(transform.position, Vector3.down, Color.red, 1, false);
 		RaycastHit2D floorHit = Physics2D.Raycast(transform.position, Vector2.down, (spriteRenderer.bounds.size.y/2)+0.1f, groundLayerMask);
@@ -93,31 +116,57 @@ public class MovementController : MonoBehaviour {
 		if (onFloor) performedDoubleJump = false;
 	}
 
-	public void Jump() {
-		if (healthController != null) {
-			if (healthController.GetHealth () <= 0) {
+	/*
+	void SetIfOnWall()
+	{
+		Debug.DrawLine(transform.position, (transform.position + (Vector3.right * ((spriteRenderer.bounds.size.y / 2) + 0.1f))), Color.green, 0, false);
+		// Debug.DrawRay(transform.position, Vector3.down, Color.red, 1, false);
+		RaycastHit2D wallHit = Physics2D.Raycast(transform.position, Vector2.right, (spriteRenderer.bounds.size.y / 2) + 0.1f, groundLayerMask);
+		onWall = (wallHit.collider != null);
+		if (onWall) performedWallJump = false;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * wallJumpDistance);
+	}
+	*/
+
+	public void Jump()
+	{
+		if (healthController != null)
+		{
+			if (healthController.GetHealth () <= 0)
+			{
 				return;
 			}
 		}
 
-		if (onFloor) {
+		if (onFloor)
+		{
 			rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
 		}
-		else if (!performedDoubleJump) {
+		else if (!performedDoubleJump)
+		{
 			performedDoubleJump = true;
 			rb2d.velocity = new Vector2(rb2d.velocity.x, doubleJumpVelocity);
 		}
 	}
 
-	public void SetSpriteDirection(Direction direction) {
-		switch (direction) {
+	public void SetSpriteDirection(Direction direction)
+	{
+		switch (direction)
+		{
 			case Direction.Left:
-				if (facingRight) {
+				if (facingRight)
+				{
 					FlipSprite();
 				}
 				break;
 			case Direction.Right:
-				if (!facingRight) {
+				if (!facingRight)
+				{
 					FlipSprite();
 				}
 				break;
@@ -126,12 +175,14 @@ public class MovementController : MonoBehaviour {
 		}
 	}
 
-	void FlipSprite() {
+	void FlipSprite()
+	{
 		spriteRenderer.flipX = !spriteRenderer.flipX;
 		facingRight = !facingRight;
 	}
 
-	public void Knockback(Vector3 force) {
+	public void Knockback(Vector3 force)
+	{
 		movementDisabled = true;
 		StartCoroutine (EnableMovement (1));
 
@@ -139,8 +190,11 @@ public class MovementController : MonoBehaviour {
 		rb2d.AddForce (force);
 	}
 
-	IEnumerator EnableMovement(float time) {
+	IEnumerator EnableMovement(float time)
+	{
 		yield return new WaitForSeconds (time);
 		movementDisabled = false;
 	}
+
+
 }
---
+++ Assets/Scripts/WallJump.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : MonoBehaviour {
+
+	public float distance = 1f;
+	public float speed = 2f;
+	MovementController movement;
+	KeyboardMovementInput controlKey;
+
+	bool wallJumping;
+
+	void Start ()
+	{
+		movement = GetComponent<MovementController>();
+	}
+	
+
+	void Update ()
+	{
+		Physics2D.queriesStartInColliders = false;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
+
+		if(Input.GetKeyDown (controlKey.defaultJumpKey) && !movement.onFloor && hit.collider !=null)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(speed * hit.normal.x, speed);
+			movement.moveVelocity = speed * hit.normal.x;
+			wallJumping = true;
+			transform.localScale = transform.localScale.x == 1 ? new Vector2(-1, 1) : Vector2.one;
+		}
+
+		else if (hit.collider != null && wallJumping)
+		{
+			wallJumping = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if ((!wallJumping || movement.onFloor))
+			movement.moveVelocity = 0;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+	}
+}
