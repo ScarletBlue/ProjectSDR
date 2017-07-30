4735a058ba7d31e94c913e17c0ad39a351658507
JejunChoi <alphacanis35@gmail.com>
2017-07-31 01:44:15 +0900

    animation key input test
    we have to fix the thing which we use bool parameter or trigger parameter when charator do specific motion

--- Assets/Scripts/AniamtionTest.cs
+++ Assets/Scripts/AniamtionTest.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class AniamtionTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.SetTrigger("Idle");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
+            animator.SetTrigger("Move");
+            animator.Play("MelonPixie_Move");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animator.SetTrigger("Dash");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            animator.SetTrigger("RisingJump");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            animator.SetTrigger("BasicAttack");
+        }
+
+        else if (Input.GetKey(KeyCode.Alpha6))
+        {
+            animator.SetBool("Skill1Bool",true);
+        }
+
+        else if (!Input.GetKey(KeyCode.Alpha6))
+        {
+            animator.SetBool("Skill1Bool", false);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            animator.SetTrigger("Utimate");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            animator.SetTrigger("BeAttacked");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            animator.SetTrigger("Die");
+        }
+    }
 }
