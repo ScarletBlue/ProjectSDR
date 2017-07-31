6095ee892cf3263ec2ee770ddf9f8667689cc36e
Song <dydwo1127@gmail.com>
2017-07-31 19:17:49 +0900

    scene1으로 작업시작, CharacterController 만드는중

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,5 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
