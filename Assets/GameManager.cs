1e86eabe21c336cdf21901d52d668e229aa372b4
JejunChoi <alphacanis35@gmail.com>
2017-08-08 22:20:29 +0900

    PointSystem

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isVictory;
+
+    // Use this for initialization
+    void Start () {
+        isVictory = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isVictory == true)
+        {
+            //EndGame();
+        }
+	}
+}
---
+++ Assets/PointSysyem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSysyem : MonoBehaviour {
+
+    GameManager gameManager;
+
+    public float healthPoint;
+    public float victoryPoint;
+    public float ultimatePoint;
+    public float healthPointMax;
+    public float victoryPointMax;
+    public float ultimatePointMax;
+
+    void Start () {
+        healthPointMax = 1000;
+        victoryPointMax = 100;
+        ultimatePointMax = 100;
+        healthPoint = 100;
+        victoryPoint = 0;
+        ultimatePoint = 0;
+	}
+	
+	void Update () {
+        if (victoryPoint == 100)
+        {
+            gameManager.isVictory = true;
+            //gameManeger에있는 승리 효과 발생
+        }
+	}
+}
