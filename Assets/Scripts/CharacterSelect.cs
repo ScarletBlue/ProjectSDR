1f1b5a82ebc649d7c97cc9d4869781b699cba078
JeongMunKyu <amigojmk@naver.com>
2017-08-15 08:36:44 +0900

    characterselect수정 및 UI수정
    chacterSelct 코드 수정
    Player cursor  UI수정

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -9,6 +9,7 @@ public class CharacterSelect : MonoBehaviour {
     int p2character;
     int p3character;
     int p4character;
+    int[] cursor;
     bool p1Choosing;
     bool p2Choosing;
     bool p3Choosing;
@@ -17,8 +18,12 @@ public class CharacterSelect : MonoBehaviour {
     public GameObject p2;
     public GameObject p3;
     public GameObject p4;
-    public int characterNum = 3;
+    [SerializeField]
+    private GameObject[] characterList;
+    public int characterNum = 8;
 
+
+    //timer
     public float selectTime = 30f;
     public float readyTime = 5f;
     public Text timer;
@@ -26,15 +31,12 @@ public class CharacterSelect : MonoBehaviour {
     int leftTimeint;
     bool moveToNextScene;
 
-	[SerializeField]
-	private GameObject[] characterList;
-
     // Use this for initialization
     void Start () {
-        p1character = 1;
+        p1character = 0;
         p2character = 1;
-        p3character = 1;
-        p4character = 1;
+        p3character = 2;
+        p4character = 3;
         p1Choosing = true;
         p2Choosing = true;
         p3Choosing = true;
@@ -52,6 +54,18 @@ public class CharacterSelect : MonoBehaviour {
         timerMove();
     }
 
+    bool isOccupied(int num)
+    {
+        if (p1character == num)
+            return true;
+        if (p2character == num)
+            return true;
+        if (p3character == num)
+            return true;
+        if (p4character == num)
+            return true;
+        return false;
+    }
 
     void playerMove()
     {
@@ -60,77 +74,81 @@ public class CharacterSelect : MonoBehaviour {
         //3p: ijkl
         //4P: arrow
         //change to joysticButton later  
-        if (InputManager.P1LeftButton() && p1Choosing)
-            if (p1character > 1)
-            {
+
+        if (p1Choosing)
+        {
+            if (InputManager.P1LeftButton() && p1character > 0 && !isOccupied(p1character - 1))
                 p1character--;
-                p1.transform.position = new Vector3(p1.transform.position.x - 150, p1.transform.position.y, p1.transform.position.z);
-            }
-        if (InputManager.P1RightButton() && p1Choosing)
-            if (p1character < characterNum)
-            {
+            if (InputManager.P1RightButton() && p1character < characterNum - 1 && !isOccupied(p1character + 1))
                 p1character++;
-                p1.transform.position = new Vector3(p1.transform.position.x + 150, p1.transform.position.y, p1.transform.position.z);
+            if (InputManager.P1UpButton() && p1character > 3 && !isOccupied(p1character - 4))
+                p1character -= 4;
+            if (InputManager.P1DownButton() && p1character < characterNum - 4 && !isOccupied(p1character + 4))
+                p1character += 4;
+            p1.transform.position = characterList[p1character].transform.position;
+            if (InputManager.P1CrossButton())
+            {
+                p1Choosing = false;
+                PlayerPrefs.SetInt("p1", p1character);
+                Debug.Log(p1character);
             }
-        if (InputManager.P1CrossButton())
-        {
-            p1Choosing = false;
-            PlayerPrefs.SetInt("p1", p1character);
-			Debug.Log(p1character);
         }
 
-        if (InputManager.P2LeftButton() && p2Choosing)
-            if (p2character > 1)
-            {
+        if (p2Choosing)
+        {
+            if (InputManager.P2LeftButton() && p2character > 0 && !isOccupied(p2character - 1))
                 p2character--;
-                p2.transform.position = new Vector3(p2.transform.position.x - 150, p2.transform.position.y, p2.transform.position.z);
-            }
-        if (InputManager.P2RightButton() && p2Choosing)
-            if (p2character < characterNum)
-            {
+            if (InputManager.P2RightButton() && p2character < characterNum - 1 && !isOccupied(p2character + 1))
                 p2character++;
-                p2.transform.position = new Vector3(p2.transform.position.x + 150, p2.transform.position.y, p2.transform.position.z);
+            if (InputManager.P2UpButton() && p2character > 3 && !isOccupied(p2character - 4))
+                p2character -= 4;
+            if (InputManager.P2DownButton() && p2character < characterNum - 4 && !isOccupied(p2character + 4))
+                p2character += 4;
+            p2.transform.position = characterList[p2character].transform.position;
+            if (InputManager.P2CrossButton())
+            {
+                p2Choosing = false;
+                PlayerPrefs.SetInt("p2", p2character);
+                Debug.Log(p2character);
             }
-        if (InputManager.P2CrossButton())
-        {
-            p2Choosing = false;
-            PlayerPrefs.SetInt("p2", p2character);
         }
 
-        if (InputManager.P3LeftButton() && p3Choosing)
-            if (p3character > 1)
-            {
+        if (p3Choosing)
+        {
+            if (InputManager.P3LeftButton() && p3character > 0 && !isOccupied(p3character - 1))
                 p3character--;
-                p3.transform.position = new Vector3(p3.transform.position.x - 150, p3.transform.position.y, p3.transform.position.z);
-            }
-        if (InputManager.P3RightButton() && p3Choosing)
-            if (p3character < characterNum)
-            {
+            if (InputManager.P3RightButton() && p3character < characterNum - 1 && !isOccupied(p3character + 1))
                 p3character++;
-                p3.transform.position = new Vector3(p3.transform.position.x + 150, p3.transform.position.y, p3.transform.position.z);
+            if (InputManager.P3UpButton() && p3character > 3 && !isOccupied(p3character - 4))
+                p3character -= 4;
+            if (InputManager.P3DownButton() && p3character < characterNum - 4 && !isOccupied(p3character + 4))
+                p3character += 4;
+            p3.transform.position = characterList[p3character].transform.position;
+            if (InputManager.P3CrossButton())
+            {
+                p3Choosing = false;
+                PlayerPrefs.SetInt("p3", p3character);
+                Debug.Log(p3character);
             }
-        if (InputManager.P3CrossButton())
-        {
-            p3Choosing = false;
-            PlayerPrefs.SetInt("p3", p3character);
         }
 
-        if (InputManager.P4LeftButton() && p4Choosing)
-            if (p4character > 1)
-            {
-                p4character--;
-                p4.transform.position = new Vector3(p4.transform.position.x - 150, p4.transform.position.y, p4.transform.position.z);
-            }
-        if (InputManager.P4RightButton() && p4Choosing)
-            if (p4character < characterNum)
-            {
-                p4character++;
-                p4.transform.position = new Vector3(p4.transform.position.x + 150, p4.transform.position.y, p4.transform.position.z);
-            }
-        if (InputManager.P4CrossButton())
+        if (p4Choosing)
         {
-            p4Choosing = false;
-            PlayerPrefs.SetInt("p4", p4character);
+            if (InputManager.P4LeftButton() && p4character > 0 && !isOccupied(p4character - 1))
+                p4character--;
+            if (InputManager.P4RightButton() && p4character < characterNum - 1 && !isOccupied(p4character + 1))
+                p4character++;
+            if (InputManager.P4UpButton() && p4character > 3 && !isOccupied(p4character - 4))
+                p4character -= 4;
+            if (InputManager.P4DownButton() && p4character < characterNum - 4 && !isOccupied(p4character + 4))
+                p4character += 4;
+            p4.transform.position = characterList[p4character].transform.position;
+            if (InputManager.P4CrossButton())
+            {
+                p4Choosing = false;
+                PlayerPrefs.SetInt("p4", p4character);
+                Debug.Log(p4character);
+            }
         }
     }
 
