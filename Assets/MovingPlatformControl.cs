f1a1420f0bdd354b44eb8b3fd9842e7c99bdebb6
Song <dydwo1127@gmail.com>
2017-08-15 20:42:33 +0900

    움직이는 플랫폼 관리하는 스크립트 삭제후 재작성, GameController에 부착된 MovingPlatformControl에서 class화 해서 관리, inspector에서 수치입력만 하면 됨

---
+++ Assets/MovingPlatformControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovingPlatformControl : MonoBehaviour {
+    
+    [System.Serializable]
+    public class MovingPlatform
+    {
+        public GameObject platform;
+        public Vector3 startPosition;
+        public Vector3 destPosition;
+
+        public bool isLooping;
+        public float speed;
+        public float respawnTime;
+        
+        public void Move()
+        {
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, destPosition, speed * Time.deltaTime);
+
+            if((destPosition - platform.transform.position).magnitude < 0.1f && isLooping)
+            {
+                platform.transform.position = startPosition;
+            }
+            else if((destPosition - platform.transform.position).magnitude < 0.1f && !isLooping)
+            {
+                Destroy(platform);
+            }
+        }
+    }
+
+    public MovingPlatform[] newPlatform;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        for (int i = 0; i < newPlatform.Length; i++)
+        {
+            newPlatform[i].Move();
+        }
+    }
+}
