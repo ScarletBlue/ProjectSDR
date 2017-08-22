c0b8dffc3906e34e889c1053a012daf4251b801e
JeongMunKyu <amigojmk@naver.com>
2017-08-22 20:34:19 +0900

    UI 타이머 추가
    전투 씬에 타이머 추가

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Text timer;
+    public float maxTime = 60;
+
+	// Use this for initialization
+	void Start () {
+        timer.text = ((int)maxTime).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        maxTime -= Time.deltaTime;
+        timer.text = ((int)maxTime).ToString();
+    }
+}
