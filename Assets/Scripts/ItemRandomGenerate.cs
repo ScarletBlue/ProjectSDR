8a7e6f1f99663f0d95ae05453e1c6364b5ac773c
JeongMunKyu <amigojmk@naver.com>
2017-08-23 04:28:50 +0900

    item random generate

---
+++ Assets/Scripts/ItemRandomGenerate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemRandomGenerate : MonoBehaviour {
+
+    public GameObject[] spawnPoints;
+    int a = 0;
+    Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        a = Random.Range(0, 10);
+        position = spawnPoints[a].GetComponent<Transform>().position;
+        transform.position = position;
+	}
+}
