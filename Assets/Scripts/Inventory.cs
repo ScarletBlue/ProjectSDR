56c0d36ef30cfd5f9c2c027a33a0a64c08a57bee
JejunChoi <alphacanis35@gmail.com>
2017-08-23 03:48:48 +0900

    bunny is imported and little win_percentage is modified

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour {
+public class Inventory : MonoBehaviour
+{
 
+    public GameObject[] inventory = new GameObject[1];
+    public bool itemAdded = false;
+    public void AddItem(GameObject item)
+    {
+        itemAdded = false;
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                inventory[i] = item;
+                Debug.Log(item.name + " was added");
+                itemAdded = true;
+                item.SendMessage("DoInteraction");
+                break;
+            }
+        }
 
-	public GameObject[] inventory = new GameObject[1];
-
-	public void AddItem(GameObject item)
-	{
-		bool itemAdded = false;
-		for (int i = 0; i < inventory.Length;  i++)
-		{
-			if(inventory [i] == null)
-			{
-				inventory[i] = item;
-				Debug.Log(item.name + " was added");
-				itemAdded = true;
-				item.SendMessage("DoInteraction");
-				break;
-			}
-		}
-
-	}
+    }
 }
