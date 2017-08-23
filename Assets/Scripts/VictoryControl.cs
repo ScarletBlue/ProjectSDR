a36f03cacd0412a657bd8a8ccfc781c27ef22ecd
JeongMunKyu <amigojmk@naver.com>
2017-08-24 04:33:58 +0900

    승리 구현

--- Assets/Scripts/VictoryControl.cs
+++ Assets/Scripts/VictoryControl.cs
@@ -20,9 +20,13 @@ public class VictoryControl : MonoBehaviour {
         //cameraZoomIn();
         
         Kim.GetComponent<CharacterControll>().enabled = false;
+        Kim.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         MP.GetComponent<CharacterControll>().enabled = false;
+        MP.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         Cat.GetComponent<CharacterControll>().enabled = false;
+        Cat.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         //Bunny.GetComponent<CharacterControll>().enabled = false;
+        //Bunny.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
         //UIPopUp;
         victoryUI.gameObject.SetActive(true);
