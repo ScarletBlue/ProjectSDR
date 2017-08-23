6a65e36088d50adb9f4d1cccd244b31a8def3ed1
JejunChoi <alphacanis35@gmail.com>
2017-08-23 14:55:55 +0900

    낙사구현

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour {
+
+    public GameObject[] spawnPoints;
+    int a = 0;
+    Vector3 position;
+
+    void Start()
+    {
+        a = Random.Range(0, 10);
+        position = spawnPoints[a].GetComponent<Transform>().position;
+        transform.position = position;
+    }
+
+    Vector3 RandomPositionSelecter()
+    {
+        a = Random.Range(0, 10);
+        return spawnPoints[a].GetComponent<Transform>().position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            //StartCoroutine(RevivePlayer(other.gameObject));
+        }
+    }
+
+    IEnumerator RevivePlayer(GameObject other)
+    {
+        yield return new WaitForSeconds(2.0f);
+        Instantiate(other, RandomPositionSelecter(), Quaternion.identity);
+    }
+}
